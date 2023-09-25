5992ce9d4d0486919a8c625b51e66a960e34942b
ellevanova <ellevanova@gmail.com>
2023-09-25 11:12:53 +0400

    added adaptive

--- public/getData.js
+++ public/getData.js
@@ -1,7 +1,6 @@
- window.estimates=null;
+window.estimates=null;
 function getData(districtClassName, realtyComplexClassName, flatClassName)  {
     if(window.estimates) {
-        console.log(districtClassName, realtyComplexClassName, flatClassName,estimates);
         replace(districtClassName, realtyComplexClassName, flatClassName,estimates);
         return;
     }
@@ -9,10 +8,7 @@ function getData(districtClassName, realtyComplexClassName, flatClassName)  {
     fetch('https://6502dc12a0f2c1f3faeafdf7.mockapi.io/test/estimates/1')
         .then(result => result.json())
         .then(itog => {
-            //console.log('itog',itog);
-            //estimates=itog;
             window.estimates=itog;
-            console.log('itog',window.estimates);
         })
 }
 
@@ -20,23 +16,17 @@ function replace(districtClassName, realtyComplexClassName, flatClassName,estima
 
     let element= document.getElementById(`${districtClassName}`);
     if(element) {
-        console.log(element)
         element.textContent = estimates.district || '';
     }
     element= document.getElementById(`${realtyComplexClassName}`);
     if(element) {
-        console.log(element)
         element.textContent = estimates["realty complex"]
-        || '';
+            || '';
     }
     element= document.getElementById(`${flatClassName}`);
     if(element) {
-        console.log(element)
         element.textContent = estimates.flate
             || '';
     }
 
 }
-/*setTimeout(()=>{
-    getData('district','realty_complex','flat')
-},2000)*/
